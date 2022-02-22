79b855b4b3571fef3321506d36906bac052ad553
ashishkumaryad50 <97010301+ashishkumaryad50@users.noreply.github.com>
2022-02-22 22:47:44 +0530

    Update revrse_string_word_Or_char_wise.js

--- revrse_string_word_Or_char_wise.js
+++ revrse_string_word_Or_char_wise.js
@@ -4,3 +4,12 @@ console.log(words)
 
 var letters= str.split('').reverse().join('')
 console.log(letters)
+
+
+//  output : hsihsa ramuk vaday  
+let str='Ashish Kumar Yadav'
+let revWords= str.split(' ')
+let newWords=revWords.map((ele)=>{
+  return ele.split('').reverse().join('')
+});
+console.log(newWords)
