bbfe0290745f9bfedd336dfbedd5d5410b6b17db
ashishkumaryad50 <97010301+ashishkumaryad50@users.noreply.github.com>
2022-02-23 00:26:39 +0530

    Create fibnacci_series.js

---
+++ fibnacci_series.js
@@ -0,0 +1,16 @@
+//0 1 1 2 3 5 8 Fabinacci Series
+let n1=0;
+let n2=1
+let n3=0;
+console.log(n1);
+console.log(n2);
+let terms=15;
+let i=0;
+while( i<terms)
+{
+ n3=n1+n2;
+  console.log(n3)
+ n1=n2;
+ n2=n3;
+ i++;
+}
