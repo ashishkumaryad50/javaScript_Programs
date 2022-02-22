99487345a6b3694b2a48998ea78b4d8dccf3762f
ashishkumaryad50 <97010301+ashishkumaryad50@users.noreply.github.com>
2022-02-22 15:22:49 +0530

    Create remove_duplicate_num_str.js

---
+++ remove_duplicate_num_str.js
@@ -0,0 +1,13 @@
+var t= [2,3,4,3,2,1]
+console.log(t)
+let data=[...new Set(t.map((e)=>(e)
+))]
+console.log(data)
+
+
+let str="sdghfhsga"
+var t= str.split('')
+console.log(t)
+let strdata=[...new Set(t.map((e)=>(e)
+))]
+console.log(strdata)
