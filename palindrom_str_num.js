28930e087b721e8a98cf9e9c6d532bfc0880c601
ashishkumaryad50 <97010301+ashishkumaryad50@users.noreply.github.com>
2022-02-22 23:26:02 +0530

    Update palindrom_str_num.js

--- palindrom_str_num.js
+++ palindrom_str_num.js
@@ -10,3 +10,20 @@ console.log("string is not Palindrom")
 }
 
 // number palindrom
+
+let n=1211
+let tmp=n;
+let rev=0;
+let rem;
+while(tmp>0){
+//if(!isNaN(tmp)){
+rem=tmp%10; //last digit
+rev=(rev*10)+rem;
+tmp=parseInt(tmp/10);
+}
+//}
+
+if(rev===n)
+  console.log("Palindrome number..");
+else  
+  console.log("not Palindrome number..");
