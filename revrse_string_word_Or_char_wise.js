d78b3490f95728b2ef509ec07fe98843e51fe331
ashishkumaryad50 <97010301+ashishkumaryad50@users.noreply.github.com>
2022-02-22 23:27:02 +0530

    Update revrse_string_word_Or_char_wise.js

--- revrse_string_word_Or_char_wise.js
+++ revrse_string_word_Or_char_wise.js
@@ -11,3 +11,20 @@ let revWords= str.split(' ').map((ele)=>{
   return ele.split('').reverse().join('')
 });
 console.log(revWords)
+
+
+//reverse of number
+
+  
+let n=1211
+let tmp=n;
+let rev=0;
+let rem;
+while(tmp>0){
+//if(!isNaN(tmp)){
+rem=tmp%10; //last digit
+rev=(rev*10)+rem;
+tmp=parseInt(tmp/10);
+}
+//}
+console.log(rev)
