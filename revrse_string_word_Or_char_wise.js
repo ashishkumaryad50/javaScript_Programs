f717800e50cde114e322b42de30ac37a1b3d1065
ashishkumaryad50 <97010301+ashishkumaryad50@users.noreply.github.com>
2022-02-22 22:31:45 +0530

    Update revrse_string_word_Or_char_wise.js

--- revrse_string_word_Or_char_wise.js
+++ revrse_string_word_Or_char_wise.js
@@ -2,5 +2,5 @@ let str="ashish kumar yadav"
 var words= str.split(' ').reverse().join(' ')
 console.log(words)
 
-var letters= str.split('').join('')
+var letters= str.split('').reverse().join('')
 console.log(letters)
