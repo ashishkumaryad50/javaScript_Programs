c00bccba68bf0eb52cb2c680827b1f34c350f388
ashishkumaryad50 <97010301+ashishkumaryad50@users.noreply.github.com>
2022-02-23 01:09:51 +0530

    Update prime_number.js

--- prime_number.js
+++ prime_number.js
@@ -6,3 +6,41 @@ for (let i = 2; i <= n; i++) { // for each i...
   }
   console.log( i ); // a prime
 }
+
+ // prime number
+let num = 12
+let nonPrime=false
+
+if(num===1 || num===0){
+  console.log("The Number is neither Prime nor non Prime ")
+}else if(num>1){
+for(let i=2;i<=num/2;i++){
+  if(num%i==0){
+  nonPrime=true;
+  break;
+  }
+  }}
+if(nonPrime==false)
+console.log("Number is Prime...");
+else
+console.log("number is non Prime...")
+
+ 
+ // prime number range
+let upperLimit=55,lowerLimit=1;
+for(let j=lowerLimit;j<=upperLimit;j++){
+let num = j
+let nonPrime=false
+if(num===1 || num===0){
+  nonPrime=true;
+	console.log("The Number is neither Prime nor non Prime ")
+}else if(num>1){
+for(let i=2;i<=num/2;i++){
+	if(num%i==0){
+  nonPrime=true;
+  break;
+  }
+  }}
+if(nonPrime==false)
+console.log(num);
+}
